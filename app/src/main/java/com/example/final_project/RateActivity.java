da747998215f97df82f4acada3264fea55083061
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-01-16 14:28:00 -0500

    Fixing stars rating

--- app/src/main/java/com/example/final_project/RateActivity.java
+++ app/src/main/java/com/example/final_project/RateActivity.java
@@ -38,16 +38,28 @@ public class RateActivity extends AppCompatActivity {
         });
 
         // initiate rating bar and a button
-        final RatingBar simpleRatingBar = (RatingBar) findViewById(R.id.simpleRatingBar);
+        final RatingBar simpleRatingBar1 = (RatingBar) findViewById(R.id.simpleRatingBar1);
+        final RatingBar simpleRatingBar2 = (RatingBar) findViewById(R.id.simpleRatingBar2);
+        final RatingBar simpleRatingBar3 = (RatingBar) findViewById(R.id.simpleRatingBar3);
+
+
         Button submitButton = (Button) findViewById(R.id.submitButton);
         // perform click event on button
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // get values and then displayed in a toast
-                String totalStars = "Total Stars:: " + simpleRatingBar.getNumStars();
-                String rating = "Rating :: " + simpleRatingBar.getRating();
-                Toast.makeText(getApplicationContext(), totalStars + "\n" + rating, Toast.LENGTH_LONG).show();
+                String totalStars1 = "Total Stars:: " + simpleRatingBar1.getNumStars();
+                String rating1 = "Rating :: " + simpleRatingBar1.getRating();
+                Toast.makeText(getApplicationContext(), totalStars1 + "\n" + rating1, Toast.LENGTH_LONG).show();
+                // get values and then displayed in a toast
+                String totalStars2 = "Total Stars:: " + simpleRatingBar2.getNumStars();
+                String rating2 = "Rating :: " + simpleRatingBar2.getRating();
+                Toast.makeText(getApplicationContext(), totalStars2 + "\n" + rating2, Toast.LENGTH_LONG).show();
+                // get values and then displayed in a toast
+                String totalStars3 = "Total Stars:: " + simpleRatingBar3.getNumStars();
+                String rating3 = "Rating :: " + simpleRatingBar3.getRating();
+                Toast.makeText(getApplicationContext(), totalStars3 + "\n" + rating3, Toast.LENGTH_LONG).show();
             }
         });
 
