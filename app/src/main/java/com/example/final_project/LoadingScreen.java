59b7dad10642601b4f417c762a01fa2cd31d517f
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-01-17 11:23:17 -0500

    Created Loading Screen
    Created a loading screen that will show when the app starts and will change to
    the main activity after a fixed about of time.

---
+++ app/src/main/java/com/example/final_project/LoadingScreen.java
@@ -0,0 +1,31 @@
+package com.example.final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        //delay in ms
+        int DELAY = 1000;
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // begin with LoadingScreen
+                Intent intent = new Intent(LoadingScreen.this, MainActivity.class);
+                // run MainActivity
+                startActivity(intent);
+            }
+        }, DELAY);
+
+    }
+}
